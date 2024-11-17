329d9da6eb55f02f02e8826f67974decc2cedc55
Andre Berezin <andreberezin@outlook.com>
2024-11-17 14:30:00 +0200

    removed unnecessary .certs

---
+++ client/src/components/lap-line-tracker/lap-line-observer.jsx
@@ -0,0 +1,8 @@
+// The Lap-line observer has been given a tablet which may be used in landscape or portrait.
+// Their interface requires 1 button for each car which will be pressed as the respective car passes the lap-line.
+// The button must simply have the car's number on it.
+// As many cars cross the lap-line quickly and often, the buttons must be very hard to miss (they must occupy a large tappable area).
+
+// Cars can still cross the lap line when the race is in finish mode.
+// The observer's display should show a message to indicate that the race session is ended once that has been declared by the Safety Official.
+// The buttons must not function after the race is ended. They should disappear or be visually disabled.
