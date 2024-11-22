43e0eb54e814a7b2bd5fd8b5568a77178971b59c
Andre Berezin <andreberezin@outlook.com>
2024-11-22 12:23:09 +0200

    modified lap line observer

--- client/src/components/lap-line-tracker/lap-line-observer.jsx
+++ client/src/components/lap-line-tracker/lap-line-observer.jsx
@@ -13,18 +13,45 @@ import "./lap-line-observer.css"
 
 import socket from "../../socket.js";
 
-// when driver finishes (car's button is pressed)
-function driverFinishedLap(setIsDisabled) {
-    socket.emit("driver finished"); // save and emit drivers lap time
-    console.log("driver finished")
-    setIsDisabled(true); // disable button when clicked todo remove this. Buttons must be disabled when the race ends (10 minutes)
-}
 
 // main function for lap line observer
 function LapLineObserver() {
-    const [raceDrivers, setRaceDrivers] = useState([]);
-    const [isDisabledArray, setIsDisabledArray] = useState([]);
 
+    const [raceDrivers, setRaceDrivers] = useState([]);
+    const [flagStatus, setFlagStatus] = useState(null)
+    const [isDisabled, setIsDisabled] = useState(() => {
+        const storedIsDisabled = localStorage.getItem("isDisabled");
+        return storedIsDisabled === "true";
+    });
+
+    // set the local storage value of "isDisabled" when isDisabled changes
+    useEffect(() => {
+        localStorage.setItem("isDisabled", isDisabled.toString());
+    }, [isDisabled]);
+
+
+    // get the flag status information form the server
+    useEffect(() => {
+        // Listen for the "broadcastFlagButtonChange" event from the server
+        socket.on("broadcastFlagButtonChange", (newFlagStatus) => {
+            console.log(newFlagStatus);
+            setFlagStatus(newFlagStatus)
+            if (newFlagStatus === "finish") { // flag status is "finish" when the timer hits 0
+                setIsDisabled(true); // Disable the buttons when the race ends
+                // stop the timer and reset the values
+            } else {
+                setIsDisabled(false) // enable the buttons again when flag status is not "finish"
+            }
+        });
+
+        // Clean up the event listener when the component unmounts
+        return () => {
+            socket.off("broadcastFlagButtonChange");
+        };
+    }, []);
+
+
+    // update race data when flagStatus changes to "safe"
     useEffect(() => {
         // Fetch the latest race data from the server
         socket.emit("getRaceData");
@@ -32,11 +59,12 @@ function LapLineObserver() {
         // get drivers' info
         const handleRaceData = (data) => {
             console.log("Received race data from server:", data);
-            setRaceDrivers(data[0].drivers)
-            //setRaceCars(data.slice(0, 8)); // max 8 drivers per race
-            // initialize all buttons as enabled initially
-            // todo limit of 8 drivers should be set on the front desk side to handle input validation there
-            setIsDisabledArray(Array(8).fill(false)); // initialize all buttons as enabled
+            // Create a new array with the "laps" property added to each driver object
+            const updatedRaceDrivers = data[0].drivers.map((driver) => ({
+                ...driver,
+                laps: 0,
+            }));
+            setRaceDrivers(updatedRaceDrivers); // change this to use status in the object instead
         };
 
         socket.on("raceData", handleRaceData);
@@ -44,33 +72,53 @@ function LapLineObserver() {
         return () => {
             socket.off("raceData", handleRaceData);
         };
-    }, []);
+    }, [flagStatus === "safe"]);
 
-    console.log(raceDrivers)
+
+    // when driver finishes a lap (or starts the first lap) (car's button is pressed)
+    function driverFinishedLap(driverName) {
+        //socket.emit("driver finished"); // save and emit drivers lap time
+
+        // Find the driver in the raceDrivers array whose name matches the driverName
+        const updatedRaceDrivers = raceDrivers.map((driver) => {
+            if (driver.name === driverName) {
+                // Increment the driver's laps by 1
+                return { ...driver, laps: driver.laps + 1 };
+            } else {
+                return driver;
+            }
+
+        });
+
+        setRaceDrivers(updatedRaceDrivers);
+
+        console.log("driver finished");
+        console.log(raceDrivers);
+        // After the Safety Official has briefed the drivers, the timer is started and the race begins.
+        // The cars set off from the pit lane once it is safe to do so.
+        // Each car is considered to start its first lap (lap 1) as it passes the lap line for the first time.
+        // Every time the car passes the lap line, the number of laps is incremented by 1,
+        // and the fastest lap time is updated if it is the fastest lap so far.
+    }
+
 
     return (
         <div className="LapLineObserver">
             <div className="container">
                 <div id="observerButtonsGrid">
                     {raceDrivers.length === 0 ? (
-                        <p>No drivers submitted yet</p>
-                    ) :
-                    raceDrivers.map((driver, index) => (
-                        <button id="observerButton"
-                                key={index}
-                                disabled={isDisabledArray[index]}
-                                className="waves-effect waves-light btn-large"
-                                onClick={() => { // todo change this logic so onClick lap time is saved
-                                    // logic to disable the correct buttons one by one and not all at once
-                                    const updatedIsDisabledArray = [...isDisabledArray];
-                                    updatedIsDisabledArray[index] = true;
-                                    setIsDisabledArray(updatedIsDisabledArray);
-                                    driverFinishedLap(() => setIsDisabledArray(updatedIsDisabledArray), index);
-                                }}
-                        >
-                            {driver.name}
-                        </button>
-                    ))}
+                            <p className={"information"}>No drivers submitted yet</p>
+                        ) :
+                        raceDrivers.map((driver, index) => (
+                            <button id="observerButton"
+                                    key={index}
+                                    className="waves-effect waves-light btn-large"
+                                    disabled={isDisabled}
+                                    onClick={() => driverFinishedLap(driver.name)}>
+                                {driver.name}
+                            </button>
+                        ))}
+                    {isDisabled && <p className={"information"}>Race session has ended</p>}
                 </div>
             </div>
         </div>
